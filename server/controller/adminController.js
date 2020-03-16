c2cfa850428c128fbe60acc3b3bbb0e51ea83bb1
afif julio <afifjulio28@gmail.com>
2020-03-16 15:04:02 +0700

    trial using jest

--- server/controller/adminController.js
+++ server/controller/adminController.js
@@ -4,7 +4,18 @@ const { Product, Admin } = require('../models/index.js')
 class adminController{
 
     static register(req,res){
-        
+        const obj = {
+            name : req.body.name,
+            email : req.body.email,
+            password : req.body.password
+        }
+        Admin.create(obj)
+        .then(result=>{
+            res.status(201).json(result)
+        })
+        .catch(err=>{
+            res.status(501).json(err)
+        })
     }
 
 }
--- server/test/comerce.test.js
+++ server/test/comerce.test.js
@@ -9,3 +9,24 @@ afterAll(done=>{
     .then(()=>done())
     .catch(err=> done(err))
 });
+
+let obj = {
+    name : "afif",
+    email : "afif@email.com",
+    password : "1234"
+}
+
+
+describe('register success', function(){
+    it('should return status 201 and result', function(done){
+        request(app)
+        .post('/user/register')
+        .send(obj)
+        .then(response =>{
+            const { body, status } = response;
+            const { result } = body;
+            expect(body).toHaveProperty('message','register success')
+            done()
+        })
+    })
+})
