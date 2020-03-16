67953aa2ba99adff1c2bed8039f979b16d4bce73
darindra23 <58590046+darindra23@users.noreply.github.com>
2020-03-16 21:27:04 +0700

    - Documentation done

--- server/app.js
+++ server/app.js
@@ -12,4 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 app.use(errorHandler);
 
+app.listen(PORT, () => console.log("Listening on port :", PORT));
+
 module.exports = app;
