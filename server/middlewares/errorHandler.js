eb0e8e80eedc301c6e0965cac8a7388070e5660a
umbone <henarivanaagit@gmail.com>
2020-03-16 17:31:06 +0700

    add a handler for status 400 and 403

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,4 +1,9 @@
 const errorHandler = (err, req, res, next) => {
+    if (err.status === 400) {
+        res.status(400).json(err)
+    } else if (err.status === 403) {
+        res.status(403).json(err)
+    }
     res.status(500).json(err)
 }
 
