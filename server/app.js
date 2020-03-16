33b8e8d7c9f90791d215d1eaa455e55b7d9c066b
stevensetiawan94 <steven.setiawan.94@gmail.com>
2020-03-16 15:05:38 +0700

    initial commit

---
+++ server/app.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const express = require ('express')
+const app = express()
+const PORT = 3000 || process.env.PORT
+const router = require('./routers')
+
+app.use(cors())
+app.use(express.urlencoded({extended:true}))
+app.use('/',router)
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
---
+++ server/models/products.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const products = sequelize.define('products', {
+    name: DataTypes.STRING,
+    image_url: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    stock: DataTypes.INTEGER
+  }, {});
+  products.associate = function(models) {
+    // associations can be defined here
+  };
+  return products;
+};
