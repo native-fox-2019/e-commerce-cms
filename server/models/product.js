d10a2c6568c98f3669cd6e855214002b65899dad
umbone <henarivanaagit@gmail.com>
2020-03-16 17:33:56 +0700

    1. Adding validation in all of User model attributes 2. Adding validation in all of Product model attributes

--- server/models/product.js
+++ server/models/product.js
@@ -1,10 +1,42 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Product = sequelize.define('Product', {
-    name: DataTypes.STRING,
-    image_url: DataTypes.STRING,
-    price: DataTypes.INTEGER,
-    stock: DataTypes.INTEGER
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "Product Name can not be empty" },
+        notEmpty: { msg: "Product Name can not be empty" }
+      }
+    },
+    image_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "Product Image Url can not be empty" },
+        notEmpty: { msg: "Product Image Url can not be empty" }
+      }
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: {
+          args: [[0]],
+          msg: "Product Price can not be negative"
+        }
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: {
+          args: [[0]],
+          msg: "Product Stock can not be negative"
+        }
+      }
+    }
   }, {});
   Product.associate = function(models) {
     // associations can be defined here
--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,39 @@
 'use strict';
+const { hash } = require('../helpers/bcrypt')
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "Name can not be empty" },
+        notEmpty: { msg: "Name can not be empty" }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "Email can not be empty" },
+        notEmpty: { msg: "Email can not be empty" }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "Password can not be empty" },
+        notEmpty: { msg: "Password can not be empty" }
+      }
+    },
     role: DataTypes.STRING
-  }, {});
+  }, {
+    hooks: {
+      beforeCreate: async (instance, options) => {
+        instance.password = await hash(instance.password)
+      }
+    }
+  });
   User.associate = function(models) {
     // associations can be defined here
   };
