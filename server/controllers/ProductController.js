faf5bc9b44264a7f3fd65df095a15126a8ed30f2
agunggst <rqz.agastya@gmail.com>
2020-03-16 19:17:59 +0700

    skeleton dan routing crud product

---
+++ server/controllers/ProductController.js
@@ -0,0 +1,27 @@
+const {
+    Product
+} = require('../models')
+
+class ProductController {
+    static create(request, response, next) {
+
+    }
+
+    static read(request, response, next) {
+
+    }
+
+    static readById(request, response, next) {
+
+    }
+
+    static update(request, response, next) {
+
+    }
+
+    static delete(request, response, next) {
+
+    }
+}
+
+module.exports = ProductController
--- server/routers/index.js
+++ server/routers/index.js
@@ -3,6 +3,6 @@ const userRouter = require('./userRouter')
 const productRouter = require('./productRouter')
 
 router.use('/user', userRouter)
-// router.use('/product', productRouter)
+router.use('/product', productRouter)
 
 module.exports = router
