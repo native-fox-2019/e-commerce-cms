d829af25926bb807156f11565e98caabaf44818b
odddyyy <56987696+odddyyy@users.noreply.github.com>
2020-03-17 12:36:37 +0700

    Adding client side

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <login v-if="!isLogin"/>
+  </div>
+</template>
+
+<script>
+import Login from './views/login.vue';
+
+export default {
+  components: {
+    Login,
+  },
+  data() {
+    return {
+      isLogin: false,
+    };
+  },
+};
+</script>
---
+++ client/src/views/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <form>
+      <label for="">Email</label>
+      <input type="text" />
+      <label for="">Password</label>
+      <input type="password" />
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+    
+};
+</script>
