5fe7392bd50f68bc0dcda88a2332370cfabd766a
Malik ibrahim Nur <222malikibrahim@gmail.com>
2020-03-17 15:53:13 +0700

    uncoment app.listen

--- server/app.js
+++ server/app.js
@@ -1,13 +1,15 @@
-
+require('dotenv').config()
 const express = require('express')
 const routes = require('./routes')
+const errorHandler = require('./middlewares/errorHandler')
+
 const app = express()
-const port =  4000
+const port = process.env.PORT || 4000
 
 app.use(express.json())
 app.use(express.urlencoded( { extended:false } ))
 app.use('/', routes)
-// error handler disini
+app.use(errorHandler)
 app.listen(port, () => {
     console.log("Listening on port "+ port)
 })
