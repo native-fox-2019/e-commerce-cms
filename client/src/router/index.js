c19be3b3aa9c005e9ca2bdccc6c41aa7338a1967
Ignatius Agus <ignatiusagus@Ignatiuss-MacBook-Pro.local>
2020-03-18 16:49:07 +0700

    Still login and register router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Products from '../views/Products.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
@@ -16,12 +17,13 @@ const routes = [
     component: Products
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/login',
+    name: 'Login',
+    component: Login
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    //component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
   }
 ]
 
---
+++ client/src/views/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="about">
+    <h1>This is admin login page</h1>
+    <form>
+        <label>Email: </label>
+        <input v-model="login.email" type="email" id="emailLogin" name="emailLogin"><br>
+        <label>Password: </label>
+        <input v-model="login.password" type="password" id="passwordLogin" name="passwordLogin"><br>
+        <input type="submit" @click="submitLogin">
+    </form>
+    <div>
+        Please register if you don't have an account
+        <button type="button" @click="registerPage">Register</button>
+    </div>
+  </div>
+</template>
+<script>
+//import axios from 'axios'
+export default{
+  data(){
+        return {
+            error: '',
+            login: {
+              email: '',
+              password: ''
+            }
+        }
+  },
+  methods:{
+    submitLogin(event){
+        event.preventDefault();
+        console.log(this.login)
+    }
+  }
+  
+
+}
+</script>
