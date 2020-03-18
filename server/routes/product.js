aa0781f80ba2c38cae2ea36c6d7576fbdd86717b
Malik ibrahim Nur <222malikibrahim@gmail.com>
2020-03-18 12:36:05 +0700

    add search product by category

--- server/routes/product.js
+++ server/routes/product.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const Controller = require('../controllers/Product')
 
 router.post('/', Controller.create)
+router.get('/search', Controller.getByCategory)
 router.get('/', Controller.getAll)
 router.get('/:id', Controller.getOne)
 router.put('/:id', Controller.update)
