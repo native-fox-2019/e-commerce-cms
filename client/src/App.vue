b0c15315ea11905b95c8df0dd156687f2aa009ef
akbarakma <akbarajo1234@gmail.com>
2020-03-18 17:59:20 +0700

    read data

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div>
     <!-- <Login /> -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <router-link to="/login"></router-link>
       <router-link to="/"></router-link>
-    </nav>
 
     <router-view></router-view>
   </div>
--- client/src/views/home.vue
+++ client/src/views/home.vue
@@ -1,5 +1,11 @@
 <template>
 <div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div>
+      <button class="btn btn-warning m-1">Log Out</button>
+      <button class="btn btn-success m-1">Add Product</button>
+    </div>
+  </nav>
   <table class="table">
   <thead class="thead-dark">
     <tr>
@@ -7,14 +13,23 @@
       <th scope="col">Image</th>
       <th scope="col">Price</th>
       <th scope="col">Stock</th>
+      <th scope="col">Action</th>
     </tr>
   </thead>
   <tbody>
     <tr v-for="data in productData" :key="data.id">
       <td>{{ data.name }}</td>
-      <td><img :src="data.image_url" :alt="data.name" style="width: 25%; height: 25%;"></td>
+      <td><img :src="data.image_url" :alt="data.name" style="width: 250px; height: 250px;"></td>
       <td>Rp. {{ data.price.toLocaleString() }}</td>
       <td>{{ data.stock }}</td>
+      <td>
+        <button class="btn btn-primary m-1">
+          Edit
+        </button>
+        <button class="btn btn-danger m-1">
+          Delete
+        </button>
+      </td>
     </tr>
   </tbody>
   </table>
