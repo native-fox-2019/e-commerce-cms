808b690021df426e0b5cc14abf767affe8461d07
odddyyy <56987696+odddyyy@users.noreply.github.com>
2020-03-19 09:34:50 +0700

    initializing deploying server to heroku

--- server/config/config.js
+++ server/config/config.js
@@ -14,11 +14,10 @@ module.exports = {
     "dialect": "postgres"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+    "username": "qdlendtwgeodpr",
+    "password": "ff18083161aa30d3fd09b2d337e475ad1109fcbc020571fff48c3a968d80d7fe",
+    "database": "d430r5vm087c8b",
+    "host": "ec2-18-213-176-229.compute-1.amazonaws.com",
+    "dialect": "postgres"
   }
 }
