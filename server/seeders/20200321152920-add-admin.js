71c5153009e2aff0f270b826854928a4a9538cfa
gigihsigap <gigihsigap@gmail.com>
2020-03-21 22:43:07 +0700

    Added seed admin data

---
+++ server/seeders/20200321152920-add-admin.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('User', [{
+      username: 'AdamSmith',
+      email: 'adamsmith@gmail.com',
+      password: 'adamsmith',
+      role: 'admin',
+    }], {});
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('People', null, {});
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
