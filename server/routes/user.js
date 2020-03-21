fe7f76046960c022aa19fbecf453160f8795f1d6
darindra23 <58590046+darindra23@users.noreply.github.com>
2020-03-21 17:58:12 +0700

    fix deployment

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,8 +5,20 @@ const { authentication } = require("../middlewares/authentication");
 const upload = require("../helpers/cloud");
 
 routes.post("/login", Controller.login);
-routes.post("/register", upload.single("file"), Controller.register);
-routes.put("/:id", authentication, user ,upload.single("file"), Controller.update);
+routes.post(
+  "/register",
+  authentication,
+  admin,
+  upload.single("file"),
+  Controller.register
+);
+routes.put(
+  "/:id",
+  authentication,
+  user,
+  upload.single("file"),
+  Controller.update
+);
 routes.get("/", authentication, Controller.get);
 routes.get("/:id", authentication, Controller.getOne);
 routes.delete("/:id", authentication, admin, Controller.delete);
