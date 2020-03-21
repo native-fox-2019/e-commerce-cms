abf1e08aaf79d1c440ab74cb3535135c2f502c22
Malik ibrahim Nur <222malikibrahim@gmail.com>
2020-03-21 15:27:25 +0700

    add cors

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const routes = require('./routes')
+const cors = require('cors')
 const errorHandler = require('./middlewares/errorHandler')
 
 const app = express()
 const port = process.env.PORT || 4000
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded( { extended:false } ))
 app.use('/', routes)
@@ -14,4 +16,4 @@ app.listen(port, () => {
     console.log("Listening on port "+ port)
 })
 
-module.exports = app
+// module.exports = app
