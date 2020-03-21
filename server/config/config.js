8eb0b477aaf8867dc6a46ec533932d80f6fe25ce
GustiPutraJarmana <gustimertaputra@gmail.com>
2020-03-22 00:30:01 +0700

    deploy

--- server/config/config.js
+++ server/config/config.js
@@ -15,11 +15,10 @@ module.exports={
     "dialect": "postgres",
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+    "username": "wdnkaumnjsgnhy",
+    "password": "b0c601f358cead28e615e39e2ced897684312944b35e95c3a0b477eea0d958fe",
+    "database": "d45kvdj3flck3q",
+    "host": "ec2-50-17-178-87.compute-1.amazonaws.com",
+    "dialect": "postgres",
   }
 }
