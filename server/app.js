0e845778acdce0cac57b693019e8bf3af6c13995
stevensetiawan94 <steven.setiawan.94@gmail.com>
2020-03-21 22:08:07 +0700

    env port

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@
 const express = require ('express')
 const app = express()
 const cors = require('cors')
-const PORT = 3000 || process.env.PORT
+const PORT = process.env.PORT||3000
 const http = require('http').createServer(app);
 const io = require('socket.io')(http)
 const router = require('./routes')
