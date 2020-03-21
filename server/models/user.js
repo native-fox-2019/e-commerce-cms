e163ce68eb89dd089e0c55a96505c38ab82e4ce3
Arif Rachman Husen <arif05rachman>
2020-03-21 21:02:49 +0700

    modified:   controllers/user.js	=> fix controller user update modified:   models/user.js	=> ""

--- server/models/user.js
+++ server/models/user.js
@@ -1,45 +1,51 @@
 'use strict';
 const {encrypt} = require('../helpers/passwordBcrypt') 
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate:{
-        notEmpty: true,
-      }
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate:{
-        notEmpty: true,
+  const User = sequelize.define(
+    "User",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
       },
-      unique: {
-        args: true,
-        msg: 'Email address already in use!'
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        },
+        unique: {
+          args: true,
+          msg: "Email address already in use!"
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
+      },
+      admin: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        validate: {
+          notEmpty: true
+        }
       }
     },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate:{
-        notEmpty: true,
-      }
-    },
-    admin: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-      validate:{
-        notEmpty: true,
-      }}
-  }, {
-    hooks: {
-      beforeCreate(user, options) {
-        user.password = encrypt(user.password)
-      }
-    },
-    sequelize});
+    {
+      hooks: {
+        beforeCreate(user, options) {
+          user.password = encrypt(user.password);
+        }
+      },
+      sequelize
+    }
+  );
   User.associate = function(models) {
     // associations can be defined here
     User.hasMany(models.Cart)
