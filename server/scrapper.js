7934a9e90c4f3738cecb92f476a4c0f4150351f7
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2020-03-26 16:16:11 +0700

    add scrapper yang lebih dinamis

--- server/scrapper.js
+++ server/scrapper.js
@@ -1,6 +1,9 @@
 const axios=require('axios')
 const fs=require('fs')
 
+var url=process.argv.slice(2)[0]
+console.log({url})
+
 function getDescription(d){
     let des=''
     let colorInfo=d.productInfo[0].productContent.colorDescription
@@ -11,11 +14,10 @@ function getDescription(d){
 
 async function startScrap(){
     try{
-        let url='https://api.nike.com/cic/browse/v1?queryid=products&anonymousId=C5F26689B209BC15034D7F00E9D08BE1&endpoint=%2Fproduct_feed%2Frollup_threads%2Fv2%3Ffilter%3Dmarketplace(ID)%26filter%3Dlanguage(en-GB)%26filter%3DemployeePrice(true)%26filter%3DattributeIds(0f64ecc7-d624-4e91-b171-b83a03dd8550%2C16633190-45e5-4830-a068-232ac7aea82c)%26anchor%3D48%26consumerChannelId%3Dd9a5bc42-4b9c-4976-858a-f159cf99c647%26count%3D60';
         let {data}=await axios.get(url)
         let dataObj=data
         let result=[];
-        let rawObjects=dataObj.data.products.objects
+        let rawObjects=dataObj.objects
         let categories=['lifestyle','gym','basketball','soccer']
 
         for(let i=0;i<rawObjects.length;i++){
@@ -39,6 +41,7 @@ async function startScrap(){
     }catch(err){
         console.log(err)
         console.log('Ada error di pada saat nge-scrap',err)
+        process.exit(-1)
     }
 }
 
